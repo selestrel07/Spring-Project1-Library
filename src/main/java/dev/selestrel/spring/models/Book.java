ae648204f2bf7002e0b39fc22de9d5d815847283
selestrel07 <selestrel07@gmail.com>
2023-12-21 13:09:27 +0400

    update models - add hibernate annotations

--- src/main/java/dev/selestrel/spring/models/Book.java
+++ src/main/java/dev/selestrel/spring/models/Book.java
@@ -1,28 +1,39 @@
 package dev.selestrel.spring.models;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
+@Entity
+@Table(name = "book")
 public class Book {
 
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private Integer personId;
     @NotNull(message = "Name should not be null")
     @Size(min = 2, max = 100, message = "Name length should be between 2 and 100")
+    @Column(name = "name")
     private String name;
     @NotNull(message = "Author name should not be null")
     @Size(min = 2, max = 200, message = "Author name length should be between 2 and 200 characters")
+    @Column(name = "author")
     private String author;
     @Max(value = 2024, message = "Adding book from future not allowed")
+    @Column(name = "year")
     private int year;
+
+    @ManyToOne
+    @JoinColumn(name = "person_id", referencedColumnName = "id")
+    private Person owner;
 
     public Book() {
     }
 
-    public Book(int id, int personId, String name, String author, int year) {
+    public Book(int id, String name, String author, int year) {
         this.id = id;
-        this.personId = personId;
         this.name = name;
         this.author = author;
         this.year = year;
@@ -34,14 +45,6 @@ public class Book {
 
     public void setId(int id) {
         this.id = id;
-    }
-
-    public Integer getPersonId() {
-        return personId;
-    }
-
-    public void setPersonId(Integer personId) {
-        this.personId = personId;
     }
 
     public String getName() {
@@ -66,5 +69,13 @@ public class Book {
 
     public void setYear(int year) {
         this.year = year;
+    }
+
+    public Person getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Person owner) {
+        this.owner = owner;
     }
 }
--- src/main/java/dev/selestrel/spring/models/Person.java
+++ src/main/java/dev/selestrel/spring/models/Person.java
@@ -1,22 +1,34 @@
 package dev.selestrel.spring.models;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
+import java.util.List;
+
+@Entity
+@Table(name = "person")
 public class Person {
 
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Size(min = 2, max = 200, message = "Name length should be between 2 and 200 characters")
+    @Column(name = "name")
     private String name;
 
     @NotNull(message = "Year of birth should not be empty")
     @Min(value = 1900, message = "Year of birth should not be before 1900")
     @Max(value = 2017, message = "Year of birth should not be after 2017 as we don't give books to person under 6")
+    @Column(name = "year_of_birth")
     private int yearOfBirth;
 
+    @OneToMany(mappedBy = "owner")
+    private List<Book> books;
     public Person() {
     }
 
@@ -48,5 +60,13 @@ public class Person {
 
     public void setYearOfBirth(int yearOfBirth) {
         this.yearOfBirth = yearOfBirth;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
     }
 }
